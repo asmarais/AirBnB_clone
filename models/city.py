082232770f40a4072de19295fe753b63dd6e616c
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-08-10 09:01:06 +0100

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+Implements City class
+'''
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    '''Defines a City'''
+
+    state_id = ""
+    name = ""

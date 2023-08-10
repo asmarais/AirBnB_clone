29334d80799335d1f4d53374c05d1be75d890469
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-08-10 10:37:27 +0100

    Update console.py

--- console.py
+++ console.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+'''Console Class
+'''
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+import cmd
 
+
+class HBNBCommand(cmd.Cmd):
+  '''the entry point of the command interpreter'''
+  
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

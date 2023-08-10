712c78c1e4e9ca31f7a2af91867a1b02574d14be
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-08-10 16:56:03 +0100

    Update console.py

--- console.py
+++ console.py
@@ -13,8 +13,30 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-  '''the entry point of the command interpreter'''
-  
+    '''the entry point of the command interpreter'''
+
+    # By default it prints (Cmd)
+    prompt = "(hbnb) "  # Prompts '(hbnb) ' on the console
+
+    def emptyline(self):
+        '''Overridden method for empty line handler'''
+
+        pass
+
+    def do_quit(self, line):
+        '''Quit command to exit the program'''
+
+        return True
+
+    def do_EOF(self, arg):
+        '''EOF command to exit the program'''
+
+        return True
+
+    def execute(self, *args, **kwargs):
+        '''command interpreter handler'''
+
+        
 
 
 if __name__ == '__main__':
